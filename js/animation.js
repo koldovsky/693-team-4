25954128884f459af63b0aa20a0ce9c74d91992d
kor4ak <malu4ina@gmail.com>
2022-05-29 18:08:13 +0000

    setted marginbuttom auto for bottle slide

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,12 @@
 
 (function() {
    const slides = [
-      '<div class="slide"><img src="img/main/main-banner-bottle.png"></div>',
-      '<div class="slide"><img src="img/main/giphy.gif"></div>' 
+      '<div class="slide"><img src="img/main/main-banner-bottle.png" alt="bottle"></div>',
+      '<div class="slide"><img src="img/main/giphy.gif" alt="glass"></div>' 
    ]; 
 
    let currentSlide = 0;
 
-//    function renderAnimation() {
-//       const slideContainer = document.querySelector('.wine-animation .slide');
-//       slideContainer.innerHTML = slides[currentSlide];
-// //    }
 function renderAnimation(){
     const slideContainer = document.querySelector('.wine-animation .slide');
     slideContainer.innerHTML = slides[currentSlide];
@@ -20,12 +16,12 @@ function renderAnimation(){
     currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
     renderAnimation();
    }
-   function prevSlide() {
-    currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
-       renderAnimation();
-   }
+//    function prevSlide() {
+//     currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
+//        renderAnimation();
+//    }
 
-//    setInterval(nextSlide, 1000);
+   // setInterval(nextSlide, 1000);
 
    renderAnimation();
 
