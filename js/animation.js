b5cea5402b69671173782fa48e2e4e0cbf23d936
kor4ak <malu4ina@gmail.com>
2022-05-30 13:51:59 +0000

    formated animation js

--- js/animation.js
+++ js/animation.js
@@ -4,34 +4,21 @@
       '<div class="slide"><img src="img/main/main-banner-bottle.png" alt="bottle"></div>',
       '<div class="slide"><img src="img/main/giphy.gif" alt="glass"></div>' 
    ]; 
-
    let currentSlide = 0;
-
 function renderAnimation(){
     const slideContainer = document.querySelector('.wine-animation .slide');
     slideContainer.innerHTML = slides[currentSlide];
 }
-
    function nextSlide() {
     currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
     renderAnimation();
    }
-   // function prevSlide() {
-   //  currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
-   //     renderAnimation();
-   // }
-
-   // setInterval(nextSlide, 1000);
-
    renderAnimation();
-   
+
    const showAnimation = document.querySelector('.wine-animation .slide');
    showAnimation.addEventListener('mouseenter', nextSlide);
-
-   // const hideAnimation = document.querySelector('.wine-animation .slide');
-   // hideAnimation.addEventListener('mouseout', prevSlide);
-
-//    window.addEventListener('resize', renderAnimation);
    
-
+   const hideAnimation = document.querySelector('.wine-animation .slide');
+   hideAnimation.addEventListener('mouseout', prevSlide);
+   
 })();
