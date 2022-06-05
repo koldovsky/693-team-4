7bdfb8b23fcc8812fd18df5bfab524cb24c37afa
Yulianna <yuliannakuzmenko@gmail.com>
2022-06-05 00:09:50 +0000

    Add js accordion animation in faq section

--- js/faq-accordion-animation.js
+++ js/faq-accordion-animation.js
@@ -1,13 +1,15 @@
-document.querySelectorAll('.accordion_button').forEach((el) => {
-    el.addEventListener('click',() => {
+(function () {
+    document.querySelectorAll('.accordion_button').forEach((el) => {
+        el.addEventListener('click',() => {
 
-        let content_acc = el.nextElementSibling;
+            let content_acc = el.nextElementSibling;
 
-        if (content_acc.style.maxHeight){
-            document.querySelectorAll('.accordion_content').forEach((el) => el.style.maxHeight = null)
-        }else{
-            document.querySelectorAll('.accordion_content').forEach((el) => el.style.maxHeight = null)
-            content_acc.style.maxHeight = content_acc.scrollHeight + 'px';
-        }
+            if (content_acc.style.maxHeight){
+                document.querySelectorAll('.accordion_content').forEach((el) => el.style.maxHeight = null)
+            }else{
+                document.querySelectorAll('.accordion_content').forEach((el) => el.style.maxHeight = null)
+                content_acc.style.maxHeight = content_acc.scrollHeight + 'px';
+            }
+        })
     })
-})
+})();
