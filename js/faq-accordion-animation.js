d2562cd7294342507d3334829d1c59bd20d639f3
Yulianna <yuliannakuzmenko@gmail.com>
2022-06-06 19:26:54 +0000

    Add accordion in faq section

--- js/faq-accordion-animation.js
+++ js/faq-accordion-animation.js
@@ -1,15 +1,29 @@
 (function () {
-    document.querySelectorAll('.accordion_button').forEach((el) => {
-        el.addEventListener('click',() => {
+    const acc_btns = document.querySelectorAll(".accordion-header");
+    const acc_contents = document.querySelectorAll(".accordion-body");
 
-            let content_acc = el.nextElementSibling;
+    acc_btns.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            acc_contents.forEach((acc) => {
+                if (
+                    e.target.nextElementSibling !== acc &&
+                    acc.classList.contains("active")
+                ) {
+                    acc.classList.remove("active");
+                    acc_btns.forEach((btn) => btn.classList.remove("active"));
+                }
+            });
 
-            if (content_acc.style.maxHeight){
-                document.querySelectorAll('.accordion_content').forEach((el) => el.style.maxHeight = null)
-            }else{
-                document.querySelectorAll('.accordion_content').forEach((el) => el.style.maxHeight = null)
-                content_acc.style.maxHeight = content_acc.scrollHeight + 'px';
-            }
-        })
-    })
-})();
+            const panel = btn.nextElementSibling;
+            panel.classList.toggle("active");
+            btn.classList.toggle("active");
+        });
+    });
+
+    window.onclick = (e) => {
+        if (!e.target.matches(".accordion-header")) {
+            acc_btns.forEach((btn) => btn.classList.remove("active"));
+            acc_contents.forEach((acc) => acc.classList.remove("active"));
+        }
+    };
+    })();
